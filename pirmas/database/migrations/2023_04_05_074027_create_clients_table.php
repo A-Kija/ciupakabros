958609b39180258efde4c33297239feecdb0faf4
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-05 11:30:37 +0300

    clients

--- pirmas/database/migrations/2023_04_05_074027_create_clients_table.php
+++ pirmas/database/migrations/2023_04_05_074027_create_clients_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->unsignedTinyInteger('tt');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('clients');
     }
 };
