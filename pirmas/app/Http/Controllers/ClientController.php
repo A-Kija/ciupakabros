119bb1e6412917fc337e3c9b1a2293f76b8fee56
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-06 10:15:21 +0300

    clients

--- pirmas/app/Http/Controllers/ClientController.php
+++ pirmas/app/Http/Controllers/ClientController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController extends Controller
 {
 
     public function index()
     {
-        //
+        $clients = Client::all()->sortBy('name');
+
+        return view('clients.index', [
+            'clients' => $clients
+        ]);
+
     }
 
 
@@ -22,36 +28,59 @@ class ClientController extends Controller
 
     public function store(Request $request)
     {
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'surname' => 'required|min:3',
+        ]);
+
+        if ($validator->fails()) {
+            $request->flash();
+            return redirect()
+                ->back()
+                ->withErrors($validator);
+        }
+        
+        
         $client = new Client;
         $client->name = $request->name;
         $client->surname = $request->surname;
         $client->tt = isset($request->tt) ? 1 : 0;
         $client->save();
-        return redirect()->back();
+        return redirect()->route('clients-index');
 
     }
 
 
     public function show(Client $client)
     {
-        //
+        return view('clients.show', [
+            'client' => $client
+        ]);
     }
 
 
     public function edit(Client $client)
     {
-        //
+        return view('clients.edit', [
+            'client' => $client
+        ]);
     }
 
 
     public function update(Request $request, Client $client)
     {
-        //
+        $client->name = $request->name;
+        $client->surname = $request->surname;
+        $client->tt = isset($request->tt) ? 1 : 0;
+        $client->save();
+        return redirect()->route('clients-index');
     }
 
 
     public function destroy(Client $client)
     {
-        //
+        $client->delete();
+        return redirect()->route('clients-index');
     }
 }
