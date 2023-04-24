d2e736d1db2442b3a5b9de7a695c8932a0abb514
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-24 11:31:33 +0300

    front o

---
+++ color-store/resources/views/front/cats.blade.php
@@ -0,0 +1,17 @@
+@inject('cats', App\Services\CatsService::class)
+<div class="card mt-5">
+    <div class="card-header">
+        <h2>Categories</h2>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+        @forelse($cats->get() as $cat)
+            <div class="cat-line">{{$cat->title}}</div>
+        @empty
+            <li class="list-group-item">
+                <div class="cat-line">No categories</div>
+            </li>   
+        @endforelse
+        </ul>
+    </div>
+</div>
