b766e7c8ca5388b53b3c337456e4611387877bbe
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-27 11:29:59 +0300

    pdf

--- color-store/app/Http/Controllers/OrderController.php
+++ color-store/app/Http/Controllers/OrderController.php
@@ -18,59 +18,13 @@ class OrderController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreOrderRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreOrderRequest $request)
+    public function update(Request $request, Order $order)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateOrderRequest  $request
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateOrderRequest $request, Order $order)
-    {
-        //
+        $order->update([
+            'status' => $request->status,
+        ]);
+        return redirect()->route('orders-index');
     }
 
     /**
--- color-store/app/Models/Order.php
+++ color-store/app/Models/Order.php
@@ -17,4 +17,9 @@ class Order extends Model
         1 => 'Proccesing',
         2 => 'Confirmed'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
